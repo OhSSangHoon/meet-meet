b6408719b260cbffd538a7105f1a013e5aae24c8
OhSSangHoon <gnsdkaos99@naver.com>
2025-05-23 11:48:31 +0900

    feat: 모달이 열릴때 레이아웃 시프트 방지

--- src/components/gatherings/CreateMeetingModal.tsx
+++ src/components/gatherings/CreateMeetingModal.tsx
@@ -50,11 +50,16 @@ export default function CreateMeetingModal({ onClose }: { onClose: () => void })
         }));
     };
     
-    // 모달이 열릴 때 스크롤 방지
+    // 모달이 열릴 때 스크롤 방지 
     useEffect(() => {
+        const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
+        
         document.body.style.overflow = 'hidden';
+        document.body.style.paddingRight = `${scrollbarWidth}px`;
+        
         return () => {
             document.body.style.overflow = 'unset';
+            document.body.style.paddingRight = '0px';
         };
     }, []);
     
